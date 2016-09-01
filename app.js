2c4ac4f23930c939f2e19547d10beff692dbf0aa
Moe Abry <moeabry@gmail.com>
2016-09-01 19:17:17 +0100

    update express for api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var cors = require('cors');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,17 +16,65 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+
+var watson = require('watson-developer-cloud');
+var natural_language_classifier = watson.natural_language_classifier({
+  url: 'https://gateway.watsonplatform.net/natural-language-classifier/api',
+  username: 'ae641e65-28bc-4835-a14e-7fa6cc104981',
+  password: 'J1EVZ0Xvq3e0',
+  version: 'v1'
+});
+
+
+
 // serve the files out of ./public as our main files
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 app.use(express.static(__dirname + '/public'));
 
+
+
+var router = express.Router(); 
+router.use(function(req, res, next) {
+    //console.log('Something is happening.');
+    next(); 
+});
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+router.route('/ask/:text')
+    .get(function(req, res) {
+      natural_language_classifier.classify({
+        text: req.params.text,
+        classifier_id: 'be05f9x94-nlc-2409' 
+      },
+      function(err, response) {
+        if (err)
+          res.send(err);
+        else
+          res.json(response);
+        });
+    });
+router.route('/status')
+    .get(function(req, res) {
+      natural_language_classifier.status({
+        classifier_id: 'be05f9x94-nlc-2409' 
+      },
+      function(err, response) {
+             if (err)
+          res.send(err);
+        else
+          res.json(response);
+        });
+    });
+app.use('/api', router);
+
+
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-//console.log(appEnv);
-
+console.log(appEnv);
 // start server on the specified port and binding host
-app.listen(1337, '0.0.0.0', function() {
+app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
--- public/js/srv/watson.js
+++ public/js/srv/watson.js
@@ -2,24 +2,15 @@
     var module = angular.module("humleyTest");
 
     var WatsonAPI = function($http, $log){
-        
-        var classificationId ="be05f9x94-nlc-2409";
-        var url = "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/"+classificationId+"/classify";
-
         var checkStatus = function(){
-            var url = "https://gateway.watsonplatform.net/natural-languge-classifier/api/v1/classifiers/be05f9x94-nlc-2409";
-            return $http.get(url).then(function(response){
+            return $http.get('/api/status')
+            .then(function(response){
                 return response.data;
             })
         }
 
         var askQuestion = function(question){
-            var data = { text: question};
-            return $http.get(url,
-            data,
-            {
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            })
+            return $http.get('/api/ask/'+question)
             .then(function(response){
                 return response.data;
             });
